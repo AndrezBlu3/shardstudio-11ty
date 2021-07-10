26470dca856d8ea1368de6577ce310276211a086
Blue <andres_pikpok@hotmail.com>
2021-07-10 03:07:06 -0500

    webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: entries,
   output: {
     path: path.resolve(__dirname, '_site/assets'),
